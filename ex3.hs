35f4904b5cf753d26a81f3f4d106a61f7afe35be
smerdyakov <73742467+smerdyakov@users.noreply.github.com>
2021-04-07 19:49:14 +0200

    ddc

---
+++ ex3.hs
@@ -0,0 +1,69 @@
+-- 9.2
+len :: [a] -> Int 
+len lst = sum x
+    where x = map justGiveMeAOne lst
+
+justGiveMeAOne :: a -> Int
+justGiveMeAOne _ = 1
+
+-- 9.4
+-- addTwo
+
+-- 9.6
+sqrs :: [Int] -> [Int]
+sqrs lst = map (^2) lst
+
+sumSqrs :: [Int] -> Int
+sumSqrs s = sum (sqrs s)
+
+greater :: [Int] -> Bool
+greater g = len ls == len g
+    where ls = filter (>0) g
+
+-- 9.7 using map, filter, zipWith
+minval :: (Int -> Int) -> Int -> Int
+minval f n = minimum x
+    where x = map f [0..n]
+
+alleq :: (Int -> Int) -> Int -> Bool
+alleq f n = and iseqf_n
+    where
+        iseqf_n = map (==(f n)) all
+        all = map f [0..n]
+
+grz :: (Int -> Int) -> Int -> Bool
+grz f n = greater (map f [0..n])
+
+ascending :: (Int -> Int) -> Int -> Bool
+ascending f n = and comp
+    where
+         comp = zipWith (<) x y 
+            where 
+                x = map f [0..(n-1)]
+                y = map f [1..n]
+
+
+
+-- test functions for 9.7
+incr :: Int -> Int 
+incr n = n+1
+
+sign :: Int -> Int
+sign n
+    | n == 0    = 0
+    | n > 0     = -n
+    | otherwise = abs n
+
+enn :: Int -> Int
+enn n = n
+
+onlyfive :: Int -> Int
+onlyfive n = 5
+
+
+-- 9.9
+iter :: Int -> (a -> a) -> a -> a
+iter 0 f x      = x
+iter n f x      = iter (n-1) f (f x)
+
+
