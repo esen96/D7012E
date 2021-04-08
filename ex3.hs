b09435079df8a54afcb9a7118f7afe0b62484460
smerdyakov <73742467+smerdyakov@users.noreply.github.com>
2021-04-08 15:47:10 +0200

    Update ex3.hs

--- ex3.hs
+++ ex3.hs
@@ -58,7 +58,7 @@ twopow n = iter (n-1) double 2
 smsqrs :: Int -> Int 
 smsqrs n = foldr (+) 0 (map (^2) [0..n])
 
---9.16
+-- 9.16
 filterFirst :: (a -> Bool) -> [a] -> [a]
 filterFirst f [] = []
 filterFirst f (x:xs)
@@ -68,3 +68,55 @@ filterFirst f (x:xs)
 -- 9.17
 filterLast :: (a -> Bool) -> [a] -> [a]
 filterLast f x = reverse (filterFirst f (reverse x))
+
+-- 10.3
+composeList :: [(a -> a)] -> (a -> a)
+composeList [] = id 
+composeList lst = foldr1 (.) lst 
+
+-- 10.5
+ins :: Int -> (Int -> Int)  
+ins = (\n -> iter n succ)
+
+test :: Int -> Int 
+test n = f n 
+    where
+        f = ins n
+
+-- 10.6
+-- \x y -> f y x
+
+-- 10.7
+reord :: (a -> b -> c) -> (b -> a -> c)
+reord f = (\x y -> f y x)
+
+-- 10.8
+nonwhite :: Char -> Bool
+nonwhite c = not (f c) 
+    where f = (\c -> elem c " \t\n")
+
+-- 10.13
+opsec :: [Int] -> [Int]
+opsec lst = (map (+1) . filter (>(-1))) lst
+
+-- 10.14
+chessBoard :: Int -> [Char]
+chessBoard n = helper 0 n
+
+helper :: Int -> Int -> [Char]
+helper i n
+    | i == n        = ""
+    | mod i 2 == 0  = whiterow 0 n ++ helper (i+1) n
+    | otherwise     = blackrow 0 n ++ helper (i+1) n
+
+whiterow :: Int -> Int -> [Char]
+whiterow i n
+    | i == n        = "\n"
+    | mod i 2 == 0  = "  " ++ whiterow (i+1) n
+    | otherwise     = "##" ++ whiterow (i+1) n
+
+blackrow :: Int -> Int -> [Char]
+blackrow i n
+    | i == n        = "\n"
+    | mod i 2 == 0  = "##" ++ blackrow (i+1) n
+    | otherwise     = "  " ++ blackrow (i+1) n
